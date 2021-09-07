1f52412fa9c2a9e79d86d9c1f1d4edaa8da35216
barghest89 <fjblechinger@gmail.com>
2021-09-07 10:13:32 +0200

    Added credits.

--- lotr.py
+++ lotr.py
@@ -2,23 +2,31 @@ import json
 import random
 
 """"
+Credits:
 Banner taken from https://www.asciiart.eu/books/lord-of-the-rings
 Credited to Blazej Kozlowski
+
+Quotes from The Lord of the Rings were originally taken from Wikiquote:
+https://en.m.wikiquote.org/wiki/The_Fellowship_of_the_Ring,
+https://en.m.wikiquote.org/wiki/The_Two_Towers,
+https://en.m.wikiquote.org/wiki/The_Return_of_the_King
+
 """
-banner = """             ___ . .  _                                                                                             
-    "T$$$P"   |  |_| |_                                                                                             
-     :$$$     |  | | |_                                                                                             
-     :$$$                                                      "T$$$$$$$b.                                          
-     :$$$     .g$$$$$p.   T$$$$b.    T$$$$$bp.                   BUG    "Tb      T$b      T$P   .g$P^^T$$  ,gP^^T$$ 
-      $$$    d^"     "^b   $$  "Tb    $$    "Tb    .s^s. :sssp   $$$     :$; T$$P $^b.     $   dP"     `T :$P    `T
-      :$$   dP         Tb  $$   :$;   $$      Tb  d'   `b $      $$$     :$;  $$  $ `Tp    $  d$           Tbp.   
-      :$$  :$;         :$; $$   :$;   $$      :$; T.   .P $^^    $$$    .dP   $$  $   ^b.  $ :$;            "T$$p.  
-      $$$  :$;         :$; $$...dP    $$      :$;  `^s^' .$.     $$$...dP"    $$  $    `Tp $ :$;     "T$$      "T$b 
-      $$$   Tb.       ,dP  $$\"""Tb    $$      dP ""$""$" "$"$^^  $$$""T$b     $$  $      ^b$  T$       T$ ;      $$;
-      $$$    Tp._   _,gP   $$   `Tb.  $$    ,dP    $  $...$ $..  $$$   T$b    :$  $       `$   Tb.     :$ T.    ,dP
-      $$$;    "^$$$$$^"   d$$     `T.d$$$$$P^"     $  $\"""$ $"", $$$    T$b  d$$bd$b      d$b   "^TbsssP" 'T$bgd$P
-      $$$b.____.dP                                 $ .$. .$.$ss,d$$$b.   T$b.
-    .d$$$$$$$$$$P  bug
+banner = """         ___ . .  _                                                                                             
+"T$$$P"   |  |_| |_                                                                                             
+ :$$$     |  | | |_                                                                                             
+ :$$$                                                      "T$$$$$$$b.                                          
+ :$$$     .g$$$$$p.   T$$$$b.    T$$$$$bp.                   BUG    "Tb      T$b      T$P   .g$P^^T$$  ,gP^^T$$ 
+  $$$    d^"     "^b   $$  "Tb    $$    "Tb    .s^s. :sssp   $$$     :$; T$$P $^b.     $   dP"     `T :$P    `T
+  :$$   dP         Tb  $$   :$;   $$      Tb  d'   `b $      $$$     :$;  $$  $ `Tp    $  d$           Tbp.   
+  :$$  :$;         :$; $$   :$;   $$      :$; T.   .P $^^    $$$    .dP   $$  $   ^b.  $ :$;            "T$$p.  
+  $$$  :$;         :$; $$...dP    $$      :$;  `^s^' .$.     $$$...dP"    $$  $    `Tp $ :$;     "T$$      "T$b 
+  $$$   Tb.       ,dP  $$\"""Tb    $$      dP ""$""$" "$"$^^  $$$""T$b     $$  $      ^b$  T$       T$ ;      $$;
+  $$$    Tp._   _,gP   $$   `Tb.  $$    ,dP    $  $...$ $..  $$$   T$b    :$  $       `$   Tb.     :$ T.    ,dP 
+  $$$;    "^$$$$$^"   d$$     `T.d$$$$$P^"     $  $\"""$ $"", $$$    T$b  d$$bd$b      d$b   "^TbsssP" 'T$bgd$P  
+  $$$b.____.dP                                 $ .$. .$.$ss,d$$$b.   T$b.                                       
+.d$$$$$$$$$$P  bug                                                    `T$b.                                     
+                                                                        "^^"       
 """
 with open('quotes.json') as f:
     data = json.load(f)
